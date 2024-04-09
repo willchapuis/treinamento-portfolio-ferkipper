98a970462821fb326bc13af130e65d464c8c5ca3
William Chapuis Marciano <william_chapuis@hotmail.com>
2024-04-09 16:15:45 -0300

    feat: config experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
 
 import "./styles/home.scss";
@@ -6,30 +7,24 @@ export default function Home() {
   return (
     <main className="container">
       <Header/>
-      <div className="experience">
-        <h3>Experience</h3>
-        <p>texto</p>
-        <div className="experience-time">
+      <Experience/>
+      <div className="infos">
+        <h3>Languages</h3>
+        <div className="languages-info">
+          <span>🇺🇸 EN - Intermediary</span>
+          <span>🇧🇷 PT-BR - Native Speaker</span>
+        </div>
+        <h3>Education</h3>
+        <div className="educational-info">
+          <span>🎓</span>
+          <span>Computer Science Bachelors Degree - Centro Universitário Eurípides de Marília</span>
+        </div>
+      </div>
+      <div className="buttons">
+        <div className="social">
 
         </div>
-        <div className="infos">
-          <h3>Languages</h3>
-          <div className="languages-info">
-            <span>🇺🇸 EN - Intermediary</span>
-            <span>🇧🇷 PT-BR - Native Speaker</span>
-          </div>
-          <h3>Education</h3>
-          <div className="educational-info">
-            <span>🎓</span>
-            <span>Computer Science Bachelors Degree - Centro Universitário Eurípides de Marília</span>
-          </div>
-        </div>
-        <div className="buttons">
-          <div className="social">
-
-          </div>
-          <button>Contact Me </button>
-        </div>
+        <button>Contact Me </button>
       </div>
     </main>
   )
