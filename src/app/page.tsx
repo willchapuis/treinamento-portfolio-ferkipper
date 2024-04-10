e7028f302b3a0973aee00270362a85750d5775f9
William Chapuis Marciano <william_chapuis@hotmail.com>
2024-04-10 15:49:05 -0300

    feat: add socials and primary button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
+import { EmailIcon } from "./components/icons/email-icon";
 import { Info } from "./components/information/information";
+import { SocialBtns } from "./components/social-btns/social-btns";
 
 import "./styles/home.scss";
 
@@ -11,10 +13,11 @@ export default function Home() {
       <Experience/>
       <Info/>
       <div className="buttons">
-        <div className="social">
-
-        </div>
-        <button className="btn-primary">Contact Me</button>
+        <SocialBtns/>
+        <a className="btn-primary" href="mailto:teste@gmail.com">
+          Contact Me
+          <EmailIcon/>
+          </button>
       </div>
     </main>
   )
