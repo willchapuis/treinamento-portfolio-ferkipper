7994d668b1c58ff06d033371cea7c736775f2fc6
William Chapuis Marciano <william_chapuis@hotmail.com>
2024-04-10 16:50:09 -0300

    feat: finish day 10 of april

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,4 +1,5 @@
 import { SectionTitle } from "../sectionTitle/section-title";
+import Image from "next/image";
 import "./experience.scss";
 
 export function Experience() {
@@ -7,7 +8,62 @@ export function Experience() {
         <SectionTitle text="Experience"></SectionTitle>
         <p>texto</p>
         <div className="experience-time">
-
+          <div className="experience-language">
+            <Image
+              src="/csharp.png"
+              alt="CSharp Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-3">
+                <span>3 Years</span>
+              </div>
+            </div>
+          </div>
+          <div className="experience-language">
+            <Image
+              src="/python.png"
+              alt="Python Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-1">
+                <span>1 Year</span>
+              </div>
+            </div>
+          </div>
+          <div className="experience-language">
+            <Image
+              src="/java.png"
+              alt="Java Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-2">
+                <span>2 Years</span>
+              </div>
+            </div>
+          </div>
+          <div className="experience-language">
+            <Image
+              src="/react.png"
+              alt="React Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure measure-1">
+                <span>1 Year</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
